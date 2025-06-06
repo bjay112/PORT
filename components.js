e5c526d23e751bfce75f65a07fbce6fb5f4c62ed
bj537 <bjeleazar922@gmail.com>
2025-06-06 16:36:59 +0800

    Update paths to use deploy directory

--- components.js
+++ components.js
@@ -12,10 +12,10 @@ async function loadComponent(elementId, componentPath) {
 // Function to initialize components
 function initializeComponents() {
     // Load navbar
-    loadComponent('navbar-container', 'navbar.html');
+    loadComponent('navbar-container', '/deploy/navbar.html');
     
     // Load footer
-    loadComponent('footer-container', 'footer.html');
+    loadComponent('footer-container', '/deploy/footer.html');
 }
 
 // Initialize components when the DOM is loaded
